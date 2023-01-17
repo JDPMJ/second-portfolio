b6737c48ef073a40c990d1890dce358b271a874f
JDPM <josedanielpmj@gmail.com>
2023-01-17 16:31:07 -0400

    change: calculator-app

--- src/components/productList/ProductList.jsx
+++ src/components/productList/ProductList.jsx
@@ -2,47 +2,13 @@ import React from "react";
 import Product from "../product/Product";
 import "./productList.css";
 import product from "../../img/product.png";
+import calculatorApp from "../../img/calculator-app-presentation.png";
 
 const projetcs = [
   {
     id: 1,
-    img: "https://github.com/JDPMJ/calculator-app/blob/master/assets/calculator-app-img-1.jpg",
+    img: calculatorApp,
     link: "https://github.com/JDPMJ/calculator-app"
-  },
-  {
-    id: 2,
-    img: product,
-    link: "https://www.google.com/"
-  },
-  {
-    id: 3,
-    img: product,
-    link: "https://www.google.com/"
-  },
-  {
-    id: 4,
-    img: product,
-    link: "https://www.google.com/"
-  },
-  {
-    id: 5,
-    img: product,
-    link: "https://www.google.com/"
-  },
-  {
-    id: 6,
-    img: product,
-    link: "https://www.google.com/"
-  },
-  {
-    id: 7,
-    img: product,
-    link: "https://www.google.com/"
-  },
-  {
-    id: 8,
-    img: product,
-    link: "https://www.google.com/"
   }
 ];
 
